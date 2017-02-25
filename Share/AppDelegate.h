65e3c01d6780e095b2caa0fd30b7a9a4546234db
Monster <Monster@wangrongrongdeMacBook-Pro.local>
2017-02-25 15:48:33 +0800

    first commit

---
+++ Share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share
+//
+//  Created by Monster on 2017/2/25.
+//  Copyright © 2017年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Share
+//
+//  Created by Monster on 2017/2/25.
+//  Copyright © 2017年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
